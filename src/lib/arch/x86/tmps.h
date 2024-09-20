babc4f5678e1b822a7a08deffc4425ab9d1ad101
Roee Shoshani <roysho10@gmail.com>
2024-09-20 14:44:19 +0300

    add explanation of tmp ids

--- src/lib/arch/x86/tmps.h
+++ src/lib/arch/x86/tmps.h
@@ -2,6 +2,8 @@
 
 #include "pis.h"
 
+/// an enum used to assign a unique id to temporaries that have special purposes in calculations.
+/// this is used to keep the offsets used in the temporary space managed, to prevent collisions.
 typedef enum {
     TMP_ID_MODRM_RM_ADDR,
     TMP_ID_SIB_INDEX,
