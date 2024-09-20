01c433ede4738a6732367713719c38399802876c
Roee Shoshani <roysho10@gmail.com>
2024-09-20 03:16:08 +0300

    remove the common.c/h files

--- src/lib/arch/x86/ctx.c
+++ src/lib/arch/x86/ctx.c
@@ -1,5 +1,4 @@
 #include "ctx.h"
-#include "arch/x86/common.h"
 #include "arch/x86/modrm.h"
 #include "errors.h"
 #include "except.h"
@@ -7,6 +6,51 @@
 #include "pis.h"
 #include "prefixes.h"
 #include "regs.h"
+
+static pis_operand_size_t get_effective_operand_size(
+    pis_x86_cpumode_t cpumode, const prefixes_t* prefixes, bool default_to_64_bit
+) {
+    bool has_size_override =
+        prefixes_contain_legacy_prefix(prefixes, LEGACY_PREFIX_OPERAND_SIZE_OVERRIDE);
+
+    switch (cpumode) {
+    case PIS_X86_CPUMODE_16_BIT:
+        return has_size_override ? PIS_OPERAND_SIZE_4 : PIS_OPERAND_SIZE_2;
+    case PIS_X86_CPUMODE_32_BIT:
+        return has_size_override ? PIS_OPERAND_SIZE_2 : PIS_OPERAND_SIZE_4;
+    case PIS_X86_CPUMODE_64_BIT:
+        if (prefixes->rex.is_present && prefixes->rex.w) {
+            return PIS_OPERAND_SIZE_8;
+        } else {
+            if (default_to_64_bit) {
+                return has_size_override ? PIS_OPERAND_SIZE_2 : PIS_OPERAND_SIZE_8;
+            } else {
+                return has_size_override ? PIS_OPERAND_SIZE_2 : PIS_OPERAND_SIZE_4;
+            }
+        }
+    default:
+        // unreachable
+        return PIS_OPERAND_SIZE_1;
+    }
+}
+
+static pis_operand_size_t
+    get_effective_addr_size(pis_x86_cpumode_t cpumode, const prefixes_t* prefixes) {
+    bool has_size_override =
+        prefixes_contain_legacy_prefix(prefixes, LEGACY_PREFIX_ADDRESS_SIZE_OVERRIDE);
+
+    switch (cpumode) {
+    case PIS_X86_CPUMODE_16_BIT:
+        return has_size_override ? PIS_OPERAND_SIZE_4 : PIS_OPERAND_SIZE_2;
+    case PIS_X86_CPUMODE_32_BIT:
+        return has_size_override ? PIS_OPERAND_SIZE_2 : PIS_OPERAND_SIZE_4;
+    case PIS_X86_CPUMODE_64_BIT:
+        return has_size_override ? PIS_OPERAND_SIZE_4 : PIS_OPERAND_SIZE_8;
+    default:
+        // unreachable
+        return PIS_OPERAND_SIZE_1;
+    }
+}
 
 static err_t post_prefixes_lift(const post_prefixes_ctx_t* ctx) {
     err_t err = SUCCESS;
@@ -82,6 +126,28 @@ static err_t lift(lift_ctx_t* ctx) {
 
 cleanup:
     return err;
+}
+
+static pis_operand_size_t cpumode_get_operand_size(pis_x86_cpumode_t cpumode) {
+    switch (cpumode) {
+    case PIS_X86_CPUMODE_64_BIT:
+        return PIS_OPERAND_SIZE_8;
+    case PIS_X86_CPUMODE_32_BIT:
+        return PIS_OPERAND_SIZE_4;
+    case PIS_X86_CPUMODE_16_BIT:
+        return PIS_OPERAND_SIZE_2;
+    default:
+        // unreachable
+        return PIS_OPERAND_SIZE_1;
+    }
+}
+
+static pis_operand_size_t get_effective_stack_addr_size(pis_x86_cpumode_t cpumode) {
+    return cpumode_get_operand_size(cpumode);
+}
+
+static pis_operand_t get_sp_operand(pis_x86_cpumode_t cpumode) {
+    return PIS_OPERAND_REG(0b100 * 8, get_effective_stack_addr_size(cpumode));
 }
 
 err_t pis_x86_lift(
