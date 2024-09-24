f32543703d2dafb01aee535e8b2362f4fd3fc4af
Roee Shoshani <roysho10@gmail.com>
2024-09-24 08:53:56 -0400

    add flags registers

--- src/lib/arch/x86/regs.h
+++ src/lib/arch/x86/regs.h
@@ -9,5 +9,27 @@ DECLARE_REG_OPERANDS(
 );
 DECLARE_REG_OPERANDS(AX, CX, DX, BX, SP, BP, SI, DI, R8W, R9W, R10W, R11W, R12W, R13W, R14W, R15W);
 
+DECLARE_REG_OPERANDS(
+    RFLAGS,
+    EFLAGS,
+    FLAGS_CF,
+    FLAGS_PF,
+    FLAGS_AF,
+    FLAGS_ZF,
+    FLAGS_SF,
+    FLAGS_TF,
+    FLAGS_IF,
+    FLAGS_DF,
+    FLAGS_OF,
+    FLAGS_IOPL,
+    FLAGS_NT,
+    FLAGS_RF,
+    FLAGS_VM,
+    FLAGS_AC,
+    FLAGS_VIF,
+    FLAGS_VIP,
+    FLAGS_ID
+);
+
 pis_operand_t
     reg_get_operand(u8 reg_encoding, pis_operand_size_t operand_size, const prefixes_t* prefixes);
