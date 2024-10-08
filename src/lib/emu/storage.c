c45e75fedca5f35170168743e38dc3ee3bcd7560
Roee Shoshani <roysho10@gmail.com>
2024-10-08 12:11:48 -0400

    refactor emu storage slot allocation

--- src/lib/emu/storage.c
+++ src/lib/emu/storage.c
@@ -4,19 +4,13 @@
 #include "pis.h"
 #include "utils.h"
 
-err_t pis_emu_storage_get_empty_slot(pis_emu_storage_t* storage, pis_emu_storage_slot_t** slot) {
+err_t pis_emu_storage_allocate_slot(pis_emu_storage_t* storage, pis_emu_storage_slot_t** slot) {
     err_t err = SUCCESS;
 
-    pis_emu_storage_slot_t* found_slot = NULL;
-    for (size_t i = 0; i < ARRAY_SIZE(storage->slots); i++) {
-        if (!storage->slots[i].is_occupied) {
-            found_slot = &storage->slots[i];
-            break;
-        }
-    }
-    CHECK_CODE(found_slot != NULL, PIS_ERR_EMU_OUT_OF_STORAGE);
+    CHECK_CODE(storage->used_slots_amount < ARRAY_SIZE(storage->slots), PIS_ERR_EMU_OUT_OF_STORAGE);
 
-    *slot = found_slot;
+    *slot = &storage->slots[storage->used_slots_amount];
+    storage->used_slots_amount++;
 
 cleanup:
     return err;
@@ -24,10 +18,7 @@ cleanup:
 
 pis_emu_storage_slot_t*
     pis_emu_storage_find_slot_by_addr(pis_emu_storage_t* storage, const pis_addr_t* addr) {
-    for (size_t i = 0; i < ARRAY_SIZE(storage->slots); i++) {
-        if (!storage->slots[i].is_occupied) {
-            continue;
-        }
+    for (size_t i = 0; i < storage->used_slots_amount; i++) {
         if (pis_addr_equals(&storage->slots[i].addr, addr)) {
             return &storage->slots[i];
         }
@@ -38,10 +29,7 @@ pis_emu_storage_slot_t*
 const pis_emu_storage_slot_t* pis_emu_storage_find_slot_by_addr_const(
     const pis_emu_storage_t* storage, const pis_addr_t* addr
 ) {
-    for (size_t i = 0; i < ARRAY_SIZE(storage->slots); i++) {
-        if (!storage->slots[i].is_occupied) {
-            continue;
-        }
+    for (size_t i = 0; i < storage->used_slots_amount; i++) {
         if (pis_addr_equals(&storage->slots[i].addr, addr)) {
             return &storage->slots[i];
         }
@@ -59,11 +47,10 @@ err_t pis_emu_storage_write_byte(
         existing_slot->byte_value = byte_value;
     } else {
         pis_emu_storage_slot_t* slot = NULL;
-        CHECK_RETHROW(pis_emu_storage_get_empty_slot(storage, &slot));
+        CHECK_RETHROW(pis_emu_storage_allocate_slot(storage, &slot));
 
         slot->addr = *addr;
         slot->byte_value = byte_value;
-        slot->is_occupied = true;
     }
 
 cleanup:
--- src/lib/emu/storage.h
+++ src/lib/emu/storage.h
@@ -4,16 +4,16 @@
 #define EMU_STORAGE_SLOTS_AMOUNT (8192)
 
 typedef struct {
-  bool is_occupied;
   pis_addr_t addr;
   u8 byte_value;
 } pis_emu_storage_slot_t;
 
 typedef struct {
   pis_emu_storage_slot_t slots[EMU_STORAGE_SLOTS_AMOUNT];
+  size_t used_slots_amount;
 } pis_emu_storage_t;
 
-err_t pis_emu_storage_get_empty_slot(pis_emu_storage_t* storage, pis_emu_storage_slot_t** slot);
+err_t pis_emu_storage_allocate_slot(pis_emu_storage_t* storage, pis_emu_storage_slot_t** slot);
 
 pis_emu_storage_slot_t* pis_emu_storage_find_slot_by_addr(pis_emu_storage_t* storage, const pis_addr_t* addr);
 
