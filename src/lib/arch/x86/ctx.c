32e0258e1a1b60a47db06ed8a413ae17a2551b8b
Roee Shoshani <roysho10@gmail.com>
2024-10-20 14:19:27 -0400

    add support for stos

--- src/lib/arch/x86/ctx.c
+++ src/lib/arch/x86/ctx.c
@@ -3117,14 +3117,39 @@ static err_t do_movs(const post_prefixes_ctx_t* ctx, pis_operand_size_t operand_
     pis_operand_t di = operand_resize(&RDI, ctx->addr_size);
 
     // copy one chunk from [si] to [di].
-    pis_operand_t byte_tmp = LIFT_CTX_NEW_TMP(ctx->lift_ctx, operand_size);
-    LIFT_CTX_EMIT(ctx->lift_ctx, PIS_INSN2(PIS_OPCODE_LOAD, byte_tmp, si));
-    LIFT_CTX_EMIT(ctx->lift_ctx, PIS_INSN2(PIS_OPCODE_STORE, di, byte_tmp));
+    pis_operand_t chunk_tmp = LIFT_CTX_NEW_TMP(ctx->lift_ctx, operand_size);
+    LIFT_CTX_EMIT(ctx->lift_ctx, PIS_INSN2(PIS_OPCODE_LOAD, chunk_tmp, si));
+    LIFT_CTX_EMIT(ctx->lift_ctx, PIS_INSN2(PIS_OPCODE_STORE, di, chunk_tmp));
 
     // increment si and di
     pis_operand_t increment =
         PIS_OPERAND_CONST(pis_operand_size_to_bytes(operand_size), ctx->addr_size);
     LIFT_CTX_EMIT(ctx->lift_ctx, PIS_INSN3(PIS_OPCODE_ADD, si, si, increment));
+    LIFT_CTX_EMIT(ctx->lift_ctx, PIS_INSN3(PIS_OPCODE_ADD, di, di, increment));
+
+    CHECK_RETHROW(rep_end(ctx, &rep_ctx));
+cleanup:
+    return err;
+}
+
+static err_t do_stos(const post_prefixes_ctx_t* ctx, pis_operand_size_t operand_size) {
+    err_t err = SUCCESS;
+
+    // movs can only be used with a REP prefix, not a REPNE prefix.
+    CHECK(group1_prefix(ctx) == LEGACY_PREFIX_REPZ_OR_REP);
+
+    rep_ctx_t rep_ctx = {};
+    CHECK_RETHROW(rep_begin(ctx, &rep_ctx));
+
+    pis_operand_t ax = operand_resize(&RSI, operand_size);
+    pis_operand_t di = operand_resize(&RDI, ctx->addr_size);
+
+    // copy one chunk from ax to [di].
+    LIFT_CTX_EMIT(ctx->lift_ctx, PIS_INSN2(PIS_OPCODE_STORE, di, ax));
+
+    // increment di
+    pis_operand_t increment =
+        PIS_OPERAND_CONST(pis_operand_size_to_bytes(operand_size), ctx->addr_size);
     LIFT_CTX_EMIT(ctx->lift_ctx, PIS_INSN3(PIS_OPCODE_ADD, di, di, increment));
 
     CHECK_RETHROW(rep_end(ctx, &rep_ctx));
@@ -3148,6 +3173,12 @@ static err_t
     } else if (first_opcode_byte == 0xa5) {
         // movs[w/d/q]
         CHECK_RETHROW(do_movs(ctx, ctx->operand_sizes.insn_default_not_64_bit));
+    } else if (first_opcode_byte == 0xaa) {
+        // stosb
+        CHECK_RETHROW(do_stos(ctx, PIS_OPERAND_SIZE_1));
+    } else if (first_opcode_byte == 0xab) {
+        // stos[w/d/q]
+        CHECK_RETHROW(do_stos(ctx, ctx->operand_sizes.insn_default_not_64_bit));
     } else {
         CHECK_FAIL_TRACE_CODE(
             PIS_ERR_UNSUPPORTED_INSN,
