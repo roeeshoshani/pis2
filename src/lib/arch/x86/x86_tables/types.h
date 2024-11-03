f922aad9d640736dafeb3869fc330bc9e9b3bcf5
Roee Shoshani <roysho10@gmail.com>
2024-11-03 17:02:55 +0200

    run x86_tables codegen

---
+++ src/lib/arch/x86/x86_tables/types.h
@@ -0,0 +1,76 @@
+#pragma once
+#include <stdint.h>
+typedef enum {MNEMONIC_CALL,MNEMONIC_IMUL,MNEMONIC_DIV,MNEMONIC_LODS,MNEMONIC_CMC,MNEMONIC_SBB,MNEMONIC_SAR,MNEMONIC_ROL,MNEMONIC_UNSUPPORTED,MNEMONIC_RET,MNEMONIC_OR,MNEMONIC_NOT,MNEMONIC_MOVSZ,MNEMONIC_AND,MNEMONIC_NEG,MNEMONIC_LEA,MNEMONIC_INC,MNEMONIC_MOV,MNEMONIC_STOS,MNEMONIC_JCC,MNEMONIC_ADC,MNEMONIC_NOP,MNEMONIC_XOR,MNEMONIC_CMP,MNEMONIC_PUSH,MNEMONIC_POP,MNEMONIC_CMD,MNEMONIC_SCAS,MNEMONIC_SHL,MNEMONIC_SHR,MNEMONIC_STC,MNEMONIC_RCR,MNEMONIC_STI,MNEMONIC_RCL,MNEMONIC_MOVS,MNEMONIC_JMP,MNEMONIC_CWD,MNEMONIC_MUL,MNEMONIC_ROR,MNEMONIC_ADD,MNEMONIC_XCHG,MNEMONIC_HLT,MNEMONIC_IDIV,MNEMONIC_CLC,MNEMONIC_CLI,MNEMONIC_CLD,MNEMONIC_STD,MNEMONIC_TEST,MNEMONIC_CMPS,MNEMONIC_SUB,MNEMONIC_DEC,MNEMONIC_MODRM_REG_OPCODE_EXT,}mnemonic_t;
+typedef union __attribute__((packed)) {
+uint8_t mnemonic: 6;
+struct __attribute__((packed)) {
+uint8_t mnemonic: 6;
+uint8_t first_op_index: 7;
+uint8_t ops_amount: 2;
+}regular;
+struct __attribute__((packed)) {
+uint8_t mnemonic: 6;
+uint8_t modrm_reg_table_index: 4;
+}modrm_reg_opcode_ext;
+}insn_info_t;
+typedef enum {OP_SIZE_8,OP_SIZE_16,OP_SIZE_32,OP_SIZE_64,}op_size_t;
+typedef struct __attribute__((packed)) {
+uint8_t with_operand_size_override: 2;
+uint8_t mode_32: 2;
+uint8_t mode_64: 2;
+uint8_t mode_64_with_rex_w: 2;
+}op_size_info_t;
+typedef enum {OP_KIND_IMM,OP_KIND_SPECIFIC_IMM,OP_KIND_REG,OP_KIND_RM,OP_KIND_SPECIFIC_REG,OP_KIND_ZEXT_SPECIFIC_REG,OP_KIND_REL,OP_KIND_MEM_OFFSET,OP_KIND_IMPLICIT,OP_KIND_COND,}op_kind_t;
+typedef enum {IMM_EXT_SIGN_EXTEND,IMM_EXT_ZERO_EXTEND,}imm_ext_kind_t;
+typedef enum {REG_ENC_MODRM,REG_ENC_OPCODE,}reg_encoding_t;
+typedef enum {SPECIFIC_REG_RAX,SPECIFIC_REG_RDX,SPECIFIC_REG_RCX,}specific_reg_t;
+typedef enum {SPECIFIC_IMM_ZERO,SPECIFIC_IMM_ONE,}specific_imm_t;
+typedef union __attribute__((packed)) {
+uint8_t kind: 4;
+struct __attribute__((packed)) {
+uint8_t kind: 4;
+uint8_t encoded_size_info_index: 3;
+uint8_t extended_size_info_index: 3;
+uint8_t extend_kind: 1;
+}imm;
+struct __attribute__((packed)) {
+uint8_t kind: 4;
+uint8_t operand_size_info_index: 3;
+uint8_t value: 1;
+}specific_imm;
+struct __attribute__((packed)) {
+uint8_t kind: 4;
+uint8_t size_info_index: 3;
+uint8_t encoding: 1;
+}reg;
+struct __attribute__((packed)) {
+uint8_t kind: 4;
+uint8_t size_info_index: 3;
+}rm;
+struct __attribute__((packed)) {
+uint8_t kind: 4;
+uint8_t size_info_index: 3;
+uint8_t reg: 2;
+}specific_reg;
+struct __attribute__((packed)) {
+uint8_t kind: 4;
+uint8_t size_info_index: 3;
+uint8_t extended_size_info_index: 3;
+uint8_t reg: 2;
+}zext_specific_reg;
+struct __attribute__((packed)) {
+uint8_t kind: 4;
+uint8_t size_info_index: 3;
+}rel;
+struct __attribute__((packed)) {
+uint8_t kind: 4;
+uint8_t mem_operand_size_info_index: 3;
+}mem_offset;
+struct __attribute__((packed)) {
+uint8_t kind: 4;
+uint8_t size_info_index: 3;
+}implicit;
+struct __attribute__((packed)) {
+uint8_t kind: 4;
+}cond;
+}op_info_t;
