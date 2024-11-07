6b7b9a294a77163cb57061821a1d0adcbdfb1ba5
Roee Shoshani <roysho10@gmail.com>
2024-11-07 21:48:41 +0200

    add an example binary which lifts hardcoded bytes

---
+++ src/examples/lift_hardcoded_bytes.c
@@ -0,0 +1,26 @@
+#include "../lib/arch/x86/ctx.h"
+#include "../lib/except.h"
+#include "../lib/pis.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+// define an example trace function
+void trace(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    vprintf(format, args);
+    va_end(args);
+}
+
+
+int main() {
+    err_t err = SUCCESS;
+    pis_x86_ctx_t ctx = {
+        .cpumode = PIS_X86_CPUMODE_64_BIT,
+    };
+    const u8 code[] = {0x48, 0x33, 0x47, 0x05};
+    pis_lift_result_t result = {};
+    CHECK_RETHROW(pis_x86_lift(&ctx, code, ARRAY_SIZE(code), 0, &result));
+cleanup:
+    return err;
+}
