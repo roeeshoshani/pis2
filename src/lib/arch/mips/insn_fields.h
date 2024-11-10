396f6f23363733ac99559489bffc0869e3abb3f3
Roee Shoshani <roysho10@gmail.com>
2024-11-11 01:52:21 +0200

    start implementing mips insn decoding

---
+++ src/lib/arch/mips/insn_fields.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "../../types.h"
+#include "../../utils.h"
+
+#define MIPS_MAX_OPCODE_VALUE ((1 << 6) - 1)
+
+static inline u8 insn_field_opcode(u32 insn) {
+    return GET_BITS(insn, 26, 6);
+}
+
+static inline u8 insn_field_rs(u32 insn) {
+    return GET_BITS(insn, 21, 5);
+}
+
+static inline u8 insn_field_rt(u32 insn) {
+    return GET_BITS(insn, 16, 5);
+}
+
+static inline u16 insn_field_offset(u32 insn) {
+    return GET_BITS(insn, 0, 16);
+}
+
+static inline u32 insn_field_instr_index(u32 insn) {
+    return GET_BITS(insn, 0, 26);
+}
+
+static inline u8 insn_field_rd(u32 insn) {
+    return GET_BITS(insn, 11, 5);
+}
+
+static inline u8 insn_field_sa(u32 insn) {
+    return GET_BITS(insn, 6, 5);
+}
+
+static inline u8 insn_field_function(u32 insn) {
+    return GET_BITS(insn, 0, 5);
+}
--- src/lib/arch/mips/lift.c
+++ src/lib/arch/mips/lift.c
@@ -1,4 +1,18 @@
 #include "lift.h"
+#include "insn_fields.h"
+
+typedef err_t (*opcode_handler_t
+)(pis_lift_args_t* args, const pis_mips_cpuinfo_t* cpuinfo, u32 insn);
+
+static err_t opcode_handler_00(pis_lift_args_t* args, const pis_mips_cpuinfo_t* cpuinfo, u32 insn) {
+    err_t err = SUCCESS;
+cleanup:
+    return err;
+}
+
+const opcode_handler_t opcode_handlers[MIPS_MAX_OPCODE_VALUE + 1] = {
+    opcode_handler_00,
+};
 
 err_t pis_mips_lift(pis_lift_args_t* args, const pis_mips_cpuinfo_t* cpuinfo) {
     err_t err = SUCCESS;
@@ -6,10 +20,16 @@ err_t pis_mips_lift(pis_lift_args_t* args, const pis_mips_cpuinfo_t* cpuinfo) {
     u32 insn = 0;
     CHECK_RETHROW(cursor_next_4(&args->machine_code, &insn, cpuinfo->endianness));
 
-    UNUSED(args);
-    UNUSED(cpuinfo);
+    u8 opcode = insn_field_opcode(insn);
+    opcode_handler_t opcode_handler = opcode_handlers[opcode];
+    CHECK_TRACE_CODE(
+        opcode_handler != NULL,
+        PIS_ERR_UNSUPPORTED_INSN,
+        "unsupported opcode 0x%2x",
+        opcode
+    );
 
-    goto cleanup;
+    CHECK_RETHROW(opcode_handler(args, cpuinfo, insn));
 
 cleanup:
     return err;
