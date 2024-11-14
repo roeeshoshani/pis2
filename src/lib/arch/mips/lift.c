8bc830b0c05263f63639b661ba695301ed576817
Roee Shoshani <roysho10@gmail.com>
2024-11-14 23:35:50 +0200

    add JAL

--- src/lib/arch/mips/lift.c
+++ src/lib/arch/mips/lift.c
@@ -1,6 +1,9 @@
 #include "lift.h"
 #include "ctx.h"
 #include "insn_fields.h"
+#include "regs.h"
+
+#define INSN_SIZE (sizeof(u32))
 
 typedef err_t (*opcode_handler_t)(ctx_t* ctx);
 
@@ -52,7 +55,7 @@ cleanup:
 }
 
 static u32 delay_slot_insn_addr(ctx_t* ctx) {
-    return ctx->args->machine_code_addr + sizeof(u32);
+    return ctx->args->machine_code_addr + INSN_SIZE;
 }
 
 static err_t lift_delay_slot_insn(ctx_t* ctx) {
@@ -62,7 +65,7 @@ static err_t lift_delay_slot_insn(ctx_t* ctx) {
     CHECK(!ctx->is_in_delay_slot);
 
     // prepare context for lifting delay slot
-    ctx->args->machine_code_addr += sizeof(u32);
+    ctx->args->machine_code_addr += INSN_SIZE;
     ctx->is_in_delay_slot = true;
     u32 orig_insn = ctx->insn;
     CHECK_RETHROW(cursor_next_4(&ctx->args->machine_code, &ctx->insn, ctx->cpuinfo->endianness));
@@ -70,20 +73,27 @@ static err_t lift_delay_slot_insn(ctx_t* ctx) {
     CHECK_RETHROW(lift(ctx));
 
     // restore context
-    ctx->args->machine_code_addr -= sizeof(u32);
+    ctx->args->machine_code_addr -= INSN_SIZE;
     ctx->is_in_delay_slot = false;
     ctx->insn = orig_insn;
 cleanup:
     return err;
 }
 
-static err_t opcode_handler_02(ctx_t* ctx) {
-    err_t err = SUCCESS;
-
-    // opcode 0x02 is J
+static u32 calc_pc_region_branch_target(ctx_t* ctx) {
     u32 addr_upper_bits = delay_slot_insn_addr(ctx) & 0xf0000000;
     u32 addr_low_bits = insn_field_instr_index(ctx->insn) << 2;
-    u32 target_addr = addr_upper_bits | addr_low_bits;
+    return addr_upper_bits | addr_low_bits;
+}
+
+static u32 calc_branch_ret_addr(ctx_t* ctx) {
+    return ctx->args->machine_code_addr + 2 * INSN_SIZE;
+}
+
+static err_t do_jmp(ctx_t* ctx) {
+    err_t err = SUCCESS;
+
+    u32 target_addr = calc_pc_region_branch_target(ctx);
 
     // run the delay slot instruction
     CHECK_RETHROW(lift_delay_slot_insn(ctx));
@@ -97,10 +107,39 @@ cleanup:
     return err;
 }
 
+static err_t opcode_handler_02(ctx_t* ctx) {
+    err_t err = SUCCESS;
+
+    // opcode 0x02 is J
+
+    CHECK_RETHROW(do_jmp(ctx));
+
+cleanup:
+    return err;
+}
+
+static err_t opcode_handler_03(ctx_t* ctx) {
+    err_t err = SUCCESS;
+
+    // opcode 0x02 is JAL
+
+    u32 ret_addr = calc_branch_ret_addr(ctx);
+    PIS_EMIT(
+        &ctx->args->result,
+        PIS_INSN2(PIS_OPCODE_MOVE, REG_RA, PIS_OPERAND_CONST(ret_addr, PIS_OPERAND_SIZE_4))
+    );
+
+    CHECK_RETHROW(do_jmp(ctx));
+
+cleanup:
+    return err;
+}
+
 static const opcode_handler_t opcode_handlers_table[MIPS_MAX_OPCODE_VALUE + 1] = {
     opcode_handler_00,
     opcode_handler_01,
     opcode_handler_02,
+    opcode_handler_03,
 };
 
 err_t pis_mips_lift(pis_lift_args_t* args, const pis_mips_cpuinfo_t* cpuinfo) {
