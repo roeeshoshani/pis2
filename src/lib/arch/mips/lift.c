9c6a2f37f84d33df9cf9f74d129a31d91bb7e7c0
Roee Shoshani <roysho10@gmail.com>
2024-11-17 00:14:57 +0200

    add MFLO and MTLO

--- src/lib/arch/mips/lift.c
+++ src/lib/arch/mips/lift.c
@@ -263,6 +263,40 @@ cleanup:
     return err;
 }
 
+static err_t special_opcode_handler_func_22(ctx_t* ctx) {
+    err_t err = SUCCESS;
+
+    // function 0x22 is MFLO
+
+    CHECK(insn_field_rs(ctx->insn) == 0);
+    CHECK(insn_field_rt(ctx->insn) == 0);
+    CHECK(insn_field_sa(ctx->insn) == 0);
+
+    pis_operand_t rd = reg_get_operand(insn_field_rd(ctx->insn));
+
+    PIS_EMIT(&ctx->args->result, PIS_INSN2(PIS_OPCODE_MOVE, rd, MIPS_REG_LO));
+
+cleanup:
+    return err;
+}
+
+static err_t special_opcode_handler_func_23(ctx_t* ctx) {
+    err_t err = SUCCESS;
+
+    // function 0x23 is MTLO
+
+    CHECK(insn_field_rt(ctx->insn) == 0);
+    CHECK(insn_field_rd(ctx->insn) == 0);
+    CHECK(insn_field_sa(ctx->insn) == 0);
+
+    pis_operand_t rs = reg_get_operand(insn_field_rs(ctx->insn));
+
+    PIS_EMIT(&ctx->args->result, PIS_INSN2(PIS_OPCODE_MOVE, MIPS_REG_LO, rs));
+
+cleanup:
+    return err;
+}
+
 static const opcode_handler_t special_opcode_func_handlers_table[MIPS_MAX_FUNCTION_VALUE + 1] = {
     [0x00] = special_opcode_handler_func_00,
     [0x02] = special_opcode_handler_func_02,
@@ -274,6 +308,8 @@ static const opcode_handler_t special_opcode_func_handlers_table[MIPS_MAX_FUNCTI
     [0x09] = special_opcode_handler_func_09,
     [0x20] = special_opcode_handler_func_20,
     [0x21] = special_opcode_handler_func_21,
+    [0x22] = special_opcode_handler_func_22,
+    [0x23] = special_opcode_handler_func_23,
 };
 
 
