c00b92eadea2119335be8be26f6f9db46634feff
Roee Shoshani <roysho10@gmail.com>
2024-11-23 17:07:31 +0200

    layout initial design for cdfg

---
+++ src/lib/cdfg.c
@@ -0,0 +1,8 @@
+#include "cdfg.h"
+
+err_t cdfg_build(cdfg_builder_t* builder, const cfg_t* cfg) {
+    err_t err = SUCCESS;
+    TODO();
+cleanup:
+    return err;
+}
--- src/lib/cdfg.h
+++ src/lib/cdfg.h
@@ -1,33 +1,45 @@
 #pragma once
 
+#include "cfg.h"
+#include "except.h"
 #include "types.h"
 
-#define PIS_CDFG_MAX_NODES 1024
-#define PIS_CDFG_MAX_EDGES 1024
+#define CDFG_MAX_NODES 1024
+#define CDFG_MAX_EDGES 1024
 
-#define PIS_CDFG_ITEM_ID_MAX (UINT16_MAX)
-#define PIS_CDFG_ITEM_ID_INVALID (PIS_CFG_ITEM_ID_MAX)
+#define CDFG_ITEM_ID_MAX (UINT16_MAX)
+#define CDFG_ITEM_ID_INVALID (CFG_ITEM_ID_MAX)
 
-typedef u16 pis_cdfg_item_id_t;
+typedef u16 cdfg_item_id_t;
 
 typedef enum {
-  PIS_CDFG_NODE_KIND_VARIABLE,
+    CDFG_NODE_KIND_VARIABLE,
 } cdfg_node_kind_t;
 
 /// represents a single node in the CDFG
 typedef struct {
+    cdfg_node_kind_t kind;
 } cdfg_node_t;
 
 /// represents a single edge in the CDFG
 typedef struct {
-  pis_cdfg_item_id_t from_node;
-  pis_cdfg_item_id_t to_node;
+    cdfg_item_id_t from_node;
+    cdfg_item_id_t to_node;
 } cdfg_edge_t;
 
+/// a control data flow graph.
 typedef struct {
-  cdfg_node_t node_storage[PIS_CDFG_MAX_NODES];
-  size_t nodes_amount;
+    cdfg_node_t node_storage[CDFG_MAX_NODES];
+    size_t nodes_amount;
 
-  cdfg_edge_t edge_storage[PIS_CDFG_MAX_EDGES];
-  size_t edges_amount;
+    cdfg_edge_t edge_storage[CDFG_MAX_EDGES];
+    size_t edges_amount;
 } cdfg_t;
+
+/// a CDFG builder.
+typedef struct {
+    /// the built CDFG.
+    cdfg_t cdfg;
+} cdfg_builder_t;
+
+err_t cdfg_build(cdfg_builder_t* builder, const cfg_t* cfg);
